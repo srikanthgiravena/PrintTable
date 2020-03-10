3815ceee9a02655d9ec90e8640e3f8fde9884555
srikanthgiravena <srikanthgiravena123@gmail.com>
2020-03-10 21:47:19 +0530

    first commit

---
+++ src/PrintTable.java
@@ -0,0 +1,16 @@
+
+public class PrintTable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		{
+			int no=17;
+			for(int i=1;i<=10;i++)
+			{
+				System.out.println(no+"*"+i+"="+no*i);
+			}
+		}
+
+	}
+
+}
